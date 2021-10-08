093b4b123e50bec6f77fcc9a1fe18348f9694517
Chacha <jonahhchacha@gmail.com>
2021-10-08 18:48:45 +0300

    Adding the forms module in our app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,7 +13,8 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     QuoteFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
